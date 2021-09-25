a6c92cc823cee15431a4c199eea3e482d18ac27f
bora001 <bora.vancouver@gmail.com>
2021-09-25 15:29:21 -0700

    heroku

--- client/src/components/views/MovieInfo/MovieInfoDetail.js
+++ client/src/components/views/MovieInfo/MovieInfoDetail.js
@@ -6,14 +6,13 @@ function MovieInfoDetail(props) {
     const { genre } = props
 
     return (
-
         <Descriptions bordered>
             <Descriptions.item label="Title">{movie.title}</Descriptions.item>
             <Descriptions.item label="Release">{movie.release_date}</Descriptions.item>
             <Descriptions.item label="Runtime">{movie.runtime}minutes</Descriptions.item>
             <Descriptions.item label="Rating">{movie.vote_average}/10</Descriptions.item>
             <Descriptions.item label="Genre">{genre.map((genre,index) => { return <span key={index}>{genre.name} </span> })}</Descriptions.item>
-        </Descriptions>
+            </Descriptions>
     )
 }
 
