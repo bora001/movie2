79bdd060152985164204b217e7d5693b0f14f05f
bora001 <bora.vancouver@gmail.com>
2021-09-27 13:23:40 -0700

    heroku

--- client/src/Config.js
+++ client/src/Config.js
@@ -1,4 +1,3 @@
-export const USER_SERVER = '/api/users'
 
 export const API_URL = 'https://api.themoviedb.org/3/'
 export const IMAGE_URL = 'https://image.tmdb.org/t/p/'
--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,13 +2,14 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 
 
-module.exports = function(app) {
+module.exports = function (app) {
 
 app.use(
 
 '/api',
 
 createProxyMiddleware({
+
 
 target: 'https://localhost:5000',
 
