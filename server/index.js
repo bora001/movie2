45638526463ff6155ae42e463f7510e72b7ce297
bora001 <bora.vancouver@gmail.com>
2021-09-27 10:49:01 -0700

    heroku

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const { auth } = require("./middleware/auth")
 const { User } = require("./models/User")
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static("../client/build"));
+    app.use(express.static("/client/build"));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname,"../client","build","index.html"))
     })
