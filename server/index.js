bb3c437020e4cb1e67efce755efbc86a886930db
bora001 <bora.vancouver@gmail.com>
2021-09-26 17:10:05 -0700

    heroku

--- server/index.js
+++ server/index.js
@@ -7,13 +7,13 @@ const config = require('./config/key')
 const { auth } = require("./middleware/auth")
 const { User } = require("./models/User")
 
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static("client/build"));
 //     app.get('*', (req, res) => {
 //         // res.sendFile(path.resolve(__dirname, "../client","build","index.html"))
 //         res.sendFile(path.resolve(__dirname, "index.html"))
 //     })
-// }
+}
 
 const port = process.env.PORT || 5000
 
