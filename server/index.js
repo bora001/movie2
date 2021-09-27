75ce38bb12d77038174ccd5a86d93645f381889b
bora001 <bora.vancouver@gmail.com>
2021-09-27 13:54:00 -0700

    heroku

--- server/index.js
+++ server/index.js
@@ -13,10 +13,11 @@ router.route('/')
         res.sendFile(path.join(__dirname+'/client/build/index.html'));
     });
 
+
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static("client/build"));
 
-    app.get('*', (req, res) => {
+    app.get('/*', (req, res) => {
         res.sendFile(path.join(__dirname,"../client","build","index.html"))
     })
 }
