cc3ddc7b2cca5b6434a2eac5dc562239368c3487
bora001 <bora.vancouver@gmail.com>
2021-09-27 10:44:59 -0700

    heroku

--- server/index.js
+++ server/index.js
@@ -1,19 +1,25 @@
 const express = require('express')
 const app = express()
-const path = require('path');
+// const path = require('path');
 // const port = 5000
 const cookieParser = require('cookie-parser')
 const config = require('./config/key')
 const { auth } = require("./middleware/auth")
 const { User } = require("./models/User")
 
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static("client/build"));
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname,"../client","build","index.html"))
+//     })
+// }
+
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static("client/build"));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname,"../client","build","index.html"))
+        res.sendFile(__dirname,"../client","build","index.html")
     })
 }
-    app.use(express.static("client/build"));
 
 
 
