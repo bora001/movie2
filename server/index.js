92b3666170e9eba230c0950be42d7e25305b0934
bora001 <bora.vancouver@gmail.com>
2021-09-27 14:17:59 -0700

    heroku

--- server/index.js
+++ server/index.js
@@ -7,17 +7,6 @@ const config = require('./config/key')
 const { auth } = require("./middleware/auth")
 const { User } = require("./models/User")
 
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static("client/build"));
-
-    app.get('/*', (req, res) => {
-        res.sendFile(path.join(__dirname,"../client","build","index.html"))
-    })
-}
-
-const port = process.env.PORT || 5000
-
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cookieParser())
@@ -109,5 +98,15 @@ app.get('/api/logout', auth, (req, res) => {
 app.post('/api/movieapi', (req, res) => {
     res.send(config.movieAPI)
 })
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static("client/build"));
+
+    app.get('/*', (req, res) => {
+        res.sendFile(path.join(__dirname,"../client","build","index.html"))
+    })
+}
+
+const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log(`localhost:${port}`))
